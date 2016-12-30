1ca03f8c5791e14cf21f33e1ec54dbbc85221325
Tomi Takussaari <tomi.takussaari@gmail.com>
2016-12-30 14:17:47 +0200

    Refactor JwtHelper

--- src/main/java/com/github/tomitakussaari/phaas/util/JwtHelper.java
+++ src/main/java/com/github/tomitakussaari/phaas/util/JwtHelper.java
@@ -1,6 +1,5 @@
 package com.github.tomitakussaari.phaas.util;
 
-import com.github.tomitakussaari.phaas.model.DataProtectionScheme;
 import com.github.tomitakussaari.phaas.model.DataProtectionScheme.CryptoData;
 import com.github.tomitakussaari.phaas.user.PhaasUserDetails;
 import com.nimbusds.jose.*;
@@ -8,7 +7,7 @@ import com.nimbusds.jose.crypto.DirectDecrypter;
 import com.nimbusds.jose.crypto.DirectEncrypter;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTClaimsSet.Builder;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.experimental.UtilityClass;
 
@@ -25,28 +24,12 @@ public class JwtHelper {
     public static String generate(PhaasUserDetails userDetails, Map<String, Object> claims) {
         try {
             CryptoData cryptoData = userDetails.currentlyActiveCryptoData();
-            JWSSigner signer = new MACSigner(cryptoData.dataProtectionKey().getBytes());
 
-            JWTClaimsSet.Builder claimSetBuilder = new JWTClaimsSet.Builder()
-                    .jwtID(UUID.randomUUID().toString())
-
-                    .issueTime(new Date());
+            Builder claimSetBuilder = new Builder().jwtID(UUID.randomUUID().toString()).issueTime(new Date());
 
             claims.forEach(claimSetBuilder::claim);
 
-            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimSetBuilder.build());
-
-            signedJWT.sign(signer);
-
-            JWEObject jweObject = new JWEObject(
-                    new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A256GCM)
-                            .keyID(String.valueOf(cryptoData.getScheme().getId()))
-                            .contentType("JWT")
-                            .build(),
-                    new Payload(signedJWT));
-
-            jweObject.encrypt(new DirectEncrypter(cryptoData.dataProtectionKey().getBytes()));
-            return jweObject.serialize();
+            return encrypt(cryptoData, signedToken(cryptoData, claimSetBuilder)).serialize();
         } catch (JOSEException e) {
             throw new JWTException(e);
         }
@@ -55,22 +38,26 @@ public class JwtHelper {
     public static Map<String, Object> verifyAndGetClaims(PhaasUserDetails userDetails, String token, Map<String, Object> requiredClaims) {
         try {
             JWEObject jweObject = JWEObject.parse(token);
-            Integer schemeId = Integer.valueOf(jweObject.getHeader().getKeyID());
-            CryptoData cryptoData = userDetails.cryptoDataForId(schemeId);
+            CryptoData cryptoData = findCryptoData(userDetails, jweObject);
 
-            jweObject.decrypt(new DirectDecrypter(cryptoData.dataProtectionKey().getBytes()));
-
-            SignedJWT signedJWT = jweObject.getPayload().toSignedJWT();
-
-            checkArgument(signedJWT.verify(new MACVerifier(cryptoData.dataProtectionKey().getBytes())), "Signature was not valid");
-
-            Map<String, Object> actualClaims = signedJWT.getJWTClaimsSet().getClaims();
+            Map<String, Object> actualClaims = decryptAndVerifySignature(jweObject, cryptoData).getJWTClaimsSet().getClaims();
 
             return verifyClaims(requiredClaims, actualClaims);
         } catch (JOSEException | ParseException | IllegalArgumentException e) {
             throw new JWTException(e);
         }
+    }
 
+    private static SignedJWT decryptAndVerifySignature(JWEObject jweObject, CryptoData cryptoData) throws JOSEException {
+        jweObject.decrypt(new DirectDecrypter(cryptoData.dataProtectionKey().getBytes()));
+        SignedJWT signedJWT = jweObject.getPayload().toSignedJWT();
+        checkArgument(signedJWT.verify(new MACVerifier(cryptoData.dataProtectionKey().getBytes())), "Signature was not valid");
+        return signedJWT;
+    }
+
+    private static CryptoData findCryptoData(PhaasUserDetails userDetails, JWEObject jweObject) {
+        Integer schemeId = Integer.valueOf(jweObject.getHeader().getKeyID());
+        return userDetails.cryptoDataForId(schemeId);
     }
 
     private static Map<String, Object> verifyClaims(Map<String, Object> requiredClaims, Map<String, Object> actualClaims) {
@@ -81,6 +68,26 @@ public class JwtHelper {
         });
         return actualClaims;
     }
+
+    private static SignedJWT signedToken(CryptoData cryptoData, Builder claimSetBuilder) throws JOSEException {
+        JWSSigner signer = new MACSigner(cryptoData.dataProtectionKey().getBytes());
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimSetBuilder.build());
+        signedJWT.sign(signer);
+        return signedJWT;
+    }
+
+    private static JWEObject encrypt(CryptoData cryptoData, SignedJWT signedJWT) throws JOSEException {
+        JWEObject jweObject = new JWEObject(
+                new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A256GCM)
+                        .keyID(String.valueOf(cryptoData.getScheme().getId()))
+                        .contentType("JWT")
+                        .build(),
+                new Payload(signedJWT));
+
+        jweObject.encrypt(new DirectEncrypter(cryptoData.dataProtectionKey().getBytes()));
+        return jweObject;
+    }
+
 
     public static class JWTException extends RuntimeException {
         JWTException(Exception e) {
